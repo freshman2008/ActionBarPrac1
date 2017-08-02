1eda51c25320a9d6ae0940c960ddf3d6c58a700a
Li Xiuliang <xiuliang.li@gi-de.com>
2017-08-02 13:39:15 +0800

    Hide ActionBar

---
+++ app/src/main/java/com/example/gd001/actionbarprac1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gd001.actionbarprac1;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
